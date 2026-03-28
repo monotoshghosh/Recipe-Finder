37d9ffc968125281c7214fcab588fb42d2640a2d
monotoshghosh <monotoshghosh49@gmail.com>
2026-03-28 19:36:47 +0530

    overlaying fixed due to targetSDK35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,14 +6,14 @@ plugins {
 
 android {
     namespace = "com.monotoshghosh.recipefinder"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.monotoshghosh.recipefinder"
         minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        targetSdk = 35
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
--- app/src/main/java/com/monotoshghosh/recipefinder/ui/components/SuccessComponent.kt
+++ app/src/main/java/com/monotoshghosh/recipefinder/ui/components/SuccessComponent.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -21,6 +22,7 @@ fun SuccessComponent(recipes: List<Meal>, onSearchClicked: (query: String) -> Un
         modifier = Modifier
             .fillMaxSize()
             .background(Color.Black) // Set background to black
+            .systemBarsPadding()
             .padding(8.dp)
     ){
         Text(
