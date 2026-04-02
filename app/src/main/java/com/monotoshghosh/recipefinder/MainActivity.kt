2f3bd085c9b38cae90d42b04d4b47ef84cc2fcbd
monotoshghosh <monotoshghosh49@gmail.com>
2026-04-02 17:03:06 +0530

    login screen added

--- app/src/main/java/com/monotoshghosh/recipefinder/MainActivity.kt
+++ app/src/main/java/com/monotoshghosh/recipefinder/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.monotoshghosh.recipefinder
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
@@ -17,46 +16,14 @@ import com.monotoshghosh.recipefinder.ui.screens.HomeScreen
 import com.monotoshghosh.recipefinder.ui.theme.RecipeFinderAppTheme
 import com.monotoshghosh.recipefinder.ui.viewmodel.RecipeViewModel
 
-import com.google.android.play.core.appupdate.AppUpdateManager
-import com.google.android.play.core.appupdate.AppUpdateManagerFactory
-import com.google.android.play.core.install.InstallStateUpdatedListener
-import com.google.android.play.core.install.model.AppUpdateType
-import com.google.android.play.core.install.model.InstallStatus
-import com.google.android.play.core.install.model.UpdateAvailability
-
+import androidx.compose.runtime.*
+import com.google.firebase.auth.FirebaseAuth
+import com.monotoshghosh.recipefinder.ui.screens.LoginScreen
 
 class MainActivity : ComponentActivity() {
     private val recipeViewModel: RecipeViewModel by viewModels()
-
-    // In-App Update Manager
-    private lateinit var appUpdateManager: AppUpdateManager
-
-    // Request code for update
-    private val updateRequestCode = 100
-
-    // Listener for update state (download complete etc.)
-    private val listener = InstallStateUpdatedListener { state ->
-        if (state.installStatus() == InstallStatus.DOWNLOADED) {
-
-            // Update downloaded in background
-            Toast.makeText(this, "Update downloaded. Restarting...", Toast.LENGTH_LONG).show()
-
-            // Complete update (installs update & restarts app)
-            appUpdateManager.completeUpdate()
-        }
-    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        // Initialize update manager
-        appUpdateManager = AppUpdateManagerFactory.create(this)
-
-        // Register listener (VERY IMPORTANT)
-        appUpdateManager.registerListener(listener)
-
-        // Check for update when app starts
-        checkForUpdate()
-
         setContent {
             RecipeFinderAppTheme {
                 // Setup system UI colors
@@ -67,39 +34,25 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    HomeScreen(recipeViewModel = recipeViewModel)
+                    var isLoggedIn by remember {
+                        mutableStateOf(FirebaseAuth.getInstance().currentUser != null)
+                    }
+
+                    if (isLoggedIn) {
+                        HomeScreen(recipeViewModel = recipeViewModel)
+                    } else {
+                        LoginScreen(
+                            onLoginSuccess = {
+                                isLoggedIn = true
+                            },
+                            onNavigateToRegister = {
+                                // later
+                            }
+                        )
+                    }
                 }
             }
         }
-    }
-
-    // Function to check update availability
-    private fun checkForUpdate() {
-
-        val appUpdateInfoTask = appUpdateManager.appUpdateInfo
-
-        appUpdateInfoTask.addOnSuccessListener { appUpdateInfo ->
-
-            // Check if update available AND flexible update allowed
-            if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
-                && appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.FLEXIBLE)
-            ) {
-
-                // Start flexible update
-                appUpdateManager.startUpdateFlowForResult(
-                    appUpdateInfo,
-                    AppUpdateType.FLEXIBLE,
-                    this,
-                    updateRequestCode
-                )
-            }
-        }
-    }
-
-    // Unregister listener (IMPORTANT to avoid memory leaks)
-    override fun onDestroy() {
-        super.onDestroy()
-        appUpdateManager.unregisterListener(listener)
     }
 
     @Composable
@@ -122,4 +75,3 @@ class MainActivity : ComponentActivity() {
 
 
 }
-
---
+++ app/src/main/java/com/monotoshghosh/recipefinder/ui/screens/LoginScreen.kt
@@ -0,0 +1,233 @@
+package com.monotoshghosh.recipefinder.ui.screens
+
+import android.widget.Toast
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.ui.text.input.VisualTransformation
+import com.google.firebase.auth.FirebaseAuth
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.res.painterResource
+import com.monotoshghosh.recipefinder.R
+
+@Composable
+fun LoginScreen(
+    onLoginSuccess: () -> Unit,
+    onNavigateToRegister: () -> Unit
+) {
+
+    val context = LocalContext.current
+    val auth = FirebaseAuth.getInstance()
+
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    var isLoading by remember { mutableStateOf(false) }
+    var isGoogleLoading by remember { mutableStateOf(false) }
+    var passwordVisible by remember { mutableStateOf(false) }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFF0B0F1C))
+    ) {
+
+        Column(
+            modifier = Modifier
+                .align(Alignment.Center)
+                .padding(horizontal = 25.dp)
+                .verticalScroll(rememberScrollState()),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            Text(
+                "Welcome Back",
+                color = Color.White,
+                fontSize = 26.sp,
+                fontWeight = FontWeight.Bold
+            )
+
+            Spacer(modifier = Modifier.height(40.dp))
+
+            // EMAIL
+            OutlinedTextField(
+                value = email,
+                onValueChange = { email = it },
+                label = { Text("Email") },
+                leadingIcon = { Icon(Icons.Default.Email, null) },
+                modifier = Modifier.fillMaxWidth(),
+                colors = OutlinedTextFieldDefaults.colors(
+                    focusedBorderColor = Color(0xFF6FE7DD),
+                    unfocusedBorderColor = Color.Transparent,
+                    focusedContainerColor = Color.White.copy(0.04f),
+                    unfocusedContainerColor = Color.White.copy(0.04f)
+                )
+            )
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            // PASSWORD
+            OutlinedTextField(
+                value = password,
+                onValueChange = { password = it },
+                label = { Text("Password") },
+                leadingIcon = { Icon(Icons.Default.Lock, null) },
+                visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+                trailingIcon = {
+                    IconButton(onClick = {
+                        passwordVisible = !passwordVisible
+                    }) {
+                        Icon(
+                            if (passwordVisible) Icons.Default.Visibility else Icons.Default.VisibilityOff,
+                            contentDescription = null
+                        )
+                    }
+                },
+                modifier = Modifier.fillMaxWidth(),
+                colors = OutlinedTextFieldDefaults.colors(
+                    focusedBorderColor = Color(0xFF6FE7DD),
+                    unfocusedBorderColor = Color.Transparent,
+                    focusedContainerColor = Color.White.copy(0.04f),
+                    unfocusedContainerColor = Color.White.copy(0.04f)
+                )
+            )
+
+            // Forgot Password
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.End
+            ) {
+                TextButton(onClick = {
+                    if (email.isEmpty()) {
+                        Toast.makeText(context, "Enter email first", Toast.LENGTH_SHORT).show()
+                        return@TextButton
+                    }
+                    FirebaseAuth.getInstance().sendPasswordResetEmail(email)
+                    Toast.makeText(context, "Reset link sent", Toast.LENGTH_SHORT).show()
+                }) {
+                    Text("Forgot Password?", color = Color.White.copy(alpha = 0.7f), fontSize = 13.sp)
+                }
+            }
+
+            Spacer(modifier = Modifier.height(10.dp))
+
+            // LOGIN BUTTON
+            Button(
+                onClick = {
+
+                    if (email.isEmpty() || password.isEmpty()) {
+                        Toast.makeText(context, "Fill all fields", Toast.LENGTH_SHORT).show()
+                        return@Button
+                    }
+
+                    if (password.length < 6) {
+                        Toast.makeText(context, "Password must be 6+ chars", Toast.LENGTH_SHORT).show()
+                        return@Button
+                    }
+
+                    isLoading = true
+
+                    auth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener {
+                            isLoading = false
+                            if (it.isSuccessful) {
+                                Toast.makeText(context, "Login Successful", Toast.LENGTH_SHORT).show()
+                                onLoginSuccess()
+                            } else {
+                                Toast.makeText(context, "Invalid Credentials", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(55.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF6FE7DD))
+            ) {
+                if (isLoading)
+                    CircularProgressIndicator(color = Color.Black)
+                else
+                    Text("Login", color = Color.Black)
+            }
+
+            Spacer(modifier = Modifier.height(25.dp))
+
+            // OR
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Divider(modifier = Modifier.weight(1f), color = Color.White.copy(0.2f))
+                Text(" OR ", color = Color.White.copy(alpha = 0.7f))
+                Divider(modifier = Modifier.weight(1f), color = Color.White.copy(0.2f))
+            }
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            Text("Continue Using", color = Color.White.copy(alpha = 0.7f))
+
+            Spacer(modifier = Modifier.height(10.dp))
+
+            // GOOGLE BUTTON (UI only for now)
+            IconButton(
+                onClick = {
+                    Toast.makeText(context, "Google Sign-In setup next", Toast.LENGTH_SHORT).show()
+                }
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.google), // add this image
+                    contentDescription = null,
+                    modifier = Modifier.size(40.dp)
+                )
+            }
+
+            Spacer(modifier = Modifier.height(25.dp))
+
+            TextButton(onClick = onNavigateToRegister) {
+                Text("Don't have an account? Sign Up", color = Color.White.copy(alpha = 0.7f))
+            }
+        }
+
+        // GOOGLE LOADING TOP INDICATOR
+        if (isGoogleLoading) {
+            Box(
+                modifier = Modifier
+                    .align(Alignment.TopCenter)
+                    .padding(top = 60.dp)
+                    .background(Color.White, shape = RoundedCornerShape(20.dp))
+                    .padding(10.dp)
+            ) {
+                CircularProgressIndicator(
+                    strokeWidth = 3.dp,
+                    color = Color(0xFF6FE7DD)
+                )
+            }
+        }
+    }
+}
